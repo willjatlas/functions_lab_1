05fa8a2511aa26bb6caae35b5ed19ccf1dd0077d
William Anderson <willjatlas@googlemail.com>
2020-11-04 17:22:59 +0000

    Completed the first two functions

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,9 @@
+# return function that returns an int of value 10. 
+def return_10():
+    return 10 
+
+# Add function that returns the sum of two int args.
+def add(num_1, num_2):
+    return num_1 + num_2
+
+ 
